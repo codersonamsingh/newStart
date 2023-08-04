c548dd91b10d4632a3b9ba25c81212dc67a6aa2c
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-04 20:57:05 +0530

    jsonwtstrategy

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const bodyparser = require("body-parser")
-const path = require("path")
-const cors = require("cors")
+const path = require("path");
+const cors = require("cors");
+const passport = require("passport");
+const cookiesession = require("cookie-Session")
 
 const app = express()
 
@@ -9,9 +11,16 @@ app.use(express.urlencoded({extended:true,limit:"50mb"}));
 app.use(bodyparser.json({limit:"50mb"}));
 app.use(express.static(path.join(__dirname,"client/build")));
 
+app.use(cookiesession([
+    maxAge:24 * 60 * 60 * 1000,
+    keys:['asdflkjhg']
+]))
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+const port = process.env.PORT ||2050;
 
-
-app.listen(2050)
-console.log("server running at 2050")
+app.listen(port, () => {
+    console.log('server is running at ${port}' )
+})
