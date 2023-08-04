c1cf7da3b10eda6d5ed4c085fa5bf8ba52e5dac5
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-04 20:20:09 +0530

    run new project

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const bodyparser = require("body-parser")
+const path = require("path")
+const cors = require("cors")
+
 const app = express()
 
-app.get("/",(req,res) => {
-    console.log("i was here")
-    res.send("i got called")
-})
+app.use(express.urlencoded({extended:true,limit:"50mb"}));
+app.use(bodyparser.json({limit:"50mb"}));
+app.use(express.static(path.join(__dirname,"client/build")));
 
 
 
