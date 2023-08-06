c8335a1d8af4e17d61c10a5c6e5b1bcb93d1e529
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-06 11:29:31 +0530

    passport session run

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const path = require("path");
 const cors = require("cors");
 const passport = require("passport");
 const cookieSession = require("cookie-session")
+const mongoose = require("mongoose")
 
 const app = express()
+
+//configure middleware
 
 app.use(express.urlencoded({extended:true,limit:"50mb"}));
 app.use(bodyparser.json({limit:"50mb"}));
 app.use(express.static(path.join(__dirname,"client/build")));
 
+//passport setup session
 app.use(cookieSession({
     maxAge:24 * 60 * 60 * 1000,
     keys:['asdflkjhg']
@@ -23,6 +27,19 @@ app.use(passport.session());
 require("./Models/User")
 require("./strategies/jsonwtStrategy")(passport)
 
+//data base connection
+
+
+
+
+
+
+
+
+
+
+
+//setup server listiening
 
 const port = process.env.PORT ||2050;
 
