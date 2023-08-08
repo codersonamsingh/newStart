07c5565a35e6201da3fc46a03c7f735b1fa3b2f3
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-08 22:37:43 +0530

    paymentvalidation module export

---
+++ validation/account/paymentValidation.js
@@ -0,0 +1,13 @@
+const validateOnCrete = async(req,res,next) => {
+
+
+
+    next();
+}
+
+const validatteOnUpdate = async(req,res,next) => {
+
+    next();
+}
+
+module.exports = {validateOnCrete, validatteOnUpdate}
