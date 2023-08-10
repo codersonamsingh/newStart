1d09fd7ae3dccac76a9c0f207795329cebd23118
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-10 20:09:15 +0530

    payment validation run

--- validation/account/paymentValidation.js
+++ validation/account/paymentValidation.js
@@ -1,6 +1,12 @@
 const validateOnCreate = async(req,res,next) => {
 
+    if(!req.body?.amount){
 
+        res.status(400).json({
+            message: "Amount Required",
+            varient: "error"
+        })
+    }
 
     next();
 }
