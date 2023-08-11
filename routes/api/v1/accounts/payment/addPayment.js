79d4c6b882074493fb3da903045fc13ca1c686c2
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-11 16:12:13 +0530

    paymentobj run

--- routes/api/v1/accounts/payment/addPayment.js
+++ routes/api/v1/accounts/payment/addPayment.js
@@ -39,6 +39,11 @@ router.post("/:id",async (req,res) => {
     try{
         const paymentObj = await getpaymentObj(req,"update")
 
+        const ayment = awit Payment.findOneAndUpdate(
+            {_id:req.params.id},
+            {$set: paymentObj},
+        )
+
 
     }catch (error) {console.log(error)
 
