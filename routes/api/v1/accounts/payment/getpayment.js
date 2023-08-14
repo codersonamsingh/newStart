65ab64e7ed2be3adbf27521b31bacb047356ce95
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-14 22:08:29 +0530

    create New payment

--- routes/api/v1/accounts/payment/getpayment.js
+++ routes/api/v1/accounts/payment/getpayment.js
@@ -1,3 +1,8 @@
 //Read = 1. Get ALL payment , 2. get one payment by Id, 3. Search Payment with note or amount
 
-const express =require("express")
+const express =require("express");
+const router = express.Router();
+
+//@type Get
+//@route /api/v1/account/payment/addpayment
+//@des Create New payment
