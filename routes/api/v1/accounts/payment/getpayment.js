94cdb2e8ceb24c45dfb81976cdf04c7bbed81994
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-14 22:13:27 +0530

    get all payment run

--- routes/api/v1/accounts/payment/getpayment.js
+++ routes/api/v1/accounts/payment/getpayment.js
@@ -4,5 +4,12 @@ const express =require("express");
 const router = express.Router();
 
 //@type Get
-//@route /api/v1/account/payment/addpayment
-//@des Create New payment
+//@route /api/v1/account/payment/getAll
+//@des get all  payment
+//@access public
+router.get("/getAll,"(req,res) => {
+
+
+})
+
+module.exports = router
