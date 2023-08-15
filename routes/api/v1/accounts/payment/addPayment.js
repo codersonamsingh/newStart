1a35110de880daf2a02e3b69b74c46939e9456c7
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-15 20:29:52 +0530

    getone payment start

--- routes/api/v1/accounts/payment/addPayment.js
+++ routes/api/v1/accounts/payment/addPayment.js
@@ -21,56 +21,7 @@ router.post("/",validateOnCreate, async(req,res) =>{
 //@access public
 router.post("/:id",async (req,res) => {
 })
-try{
-    const paymentObj = await getpaymentObj(req,"create")
 
-
-    await new Payment(paymentObj)
-    .save();
-
-    res.status(201).json({
-        message: "Payment Added",
-        varient: "success"
-    })
-}catch(error) {
-    console.log(error)
-
-    res.status(500).json({
-        message: "Internal Server error",
-        varient: "error"
-    })
-}
-
-
-
-    try{
-        const paymentObj = await getpaymentObj(req,"update")
-
-        const payment = await Payment.findOneAndUpdate(
-            {_id:req.params.id},
-            {$set: paymentObj},
-            {new:true}
-        )
-
-        if(!Payment){
-            res.status(500).json({
-                message: "payment not found",
-                varient: "error"
-            })
-        }
-
-        res.status(201).json({
-            message: "Payment Successfully",
-            varient: "success"
-        })
-
-    }catch (error) {console.log(error)
-
-        res.status(500).json({
-            message: "Internal Server error",
-            varient: "error"
-        }) 
-    }
 //@type Delete
 //@route / api/v1/accounts/payment/addpayment/deleteOne/id:
 //@des Delete payment
@@ -79,31 +30,7 @@ try{
 router.delete("/deleteOne/:id",async(req,res) => {
     })
 
-    try{
-
-        const Payment = await Payment.findByIdAndRemove(req.params.id);
-        if(!Payment){
-            res.status(500).json({
-                message: "payment not found",
-                varient: "error"
-            })
-        }
-
-        res.status(201).json({
-            message: "Payment Deleted Successfully",
-            varient: "success"
-        })
-
-    }
-    catch(error){
-        console.log(error)
-
-        res.status(500).json({
-            message: "Internal Server error",
-            varient: "error"
-        })
-    }
-
+    
 async function getpaymentObj(req,type){
 
     let newPayment = {}
