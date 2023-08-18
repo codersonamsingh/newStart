0b445a201a22b7ace6840e93f31d664e16078d86
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-18 23:15:23 +0530

    document run

---
+++ Models/Accounts/Document.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const ReceiptSchema = new Schema({
+   
+    receiptNumber:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        default:""
+    },
+    date:{
+        type:Date,
+        default: Date.now
+    }
+});
+module.exports = Receipt = mongoose.model("Receipt",ReceiptSchema)5ty       srt5fggggggggggggggggggggg67y5t
