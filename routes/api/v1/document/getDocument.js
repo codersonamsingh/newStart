eee1ba6806050d57a4bf548e442925cc3d889bf5
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-19 19:42:29 +0530

    getDocument running

--- routes/api/v1/document/getDocument.js
+++ routes/api/v1/document/getDocument.js
@@ -1,23 +1,23 @@
-//Read = 1. Get ALL receipt , 2. get one receipt by Id, 3. Search Receipt with note or amount
+//Read = 1. Get ALL document , 2. get one document by Id, 3. Search Document with note or amount
 
 const express = require("express");
-const Receipt = require("../../../../../Models/Accounts/Receipt");
+const Document = require("../../../../../Models/Accounts/Document");
 const router = express.Router();
 
 //@type Get
-//@route /api/v1/account/receipt/getreceipt/getAll
-//@des get all  receipt
+//@route /api/v1/account/document/getdocument/getAll
+//@des get all  document
 //@access public
 router.get("/getAll", async(req,res) => {
 
 try{ 
     
-    const getReceipt = await Receipt.find()
+    const getDocument = await Document.find()
 
 
      res.json({
-        data:getreceipt,
-        message:"receipt loaded",
+        data:getdocument,
+        message:"document loaded",
         varient:"success"
      })
 
@@ -32,19 +32,19 @@ try{
 })
 
 //@type Get
-//@route /api/v1/account/receipt/getreceipt/getOne/:id
-//@des get one receipt
+//@route /api/v1/account/document/getdocument/getOne/:id
+//@des get one document
 //@access public
 router.get("/getOne/:id", async(req,res) => {
 
     try{ 
         
-        const getReceipt = await Receipt.findById(req.params.id)
+        const getDocument = await Document.findById(req.params.id)
     
     
          res.json({
-            data:getreceipt,
-            message:"receipt loaded",
+            data:getdocument,
+            message:"document loaded",
             varient:"success"
          })
     
@@ -59,8 +59,8 @@ router.get("/getOne/:id", async(req,res) => {
     })
 
 //@type Get search
-//@route /api/v1/account/receipt/getreceipt/search/:seachQuery
-//@des to search receipt
+//@route /api/v1/account/document/getdocument/search/:seachQuery
+//@des to search document
 //@access public
 router.get("/search/:searchQuery", async(req,res) => {
 
@@ -68,16 +68,16 @@ router.get("/search/:searchQuery", async(req,res) => {
         
         const searchQuery = req.params.searchQuery
 
-        const getReceipt = await Receipt.find({
+        const getDocument = await Document.find({
             $or:[
-                {receiptNumber: new RegExp(searchQuery, "i")},
+                {documentNumber: new RegExp(searchQuery, "i")},
                 {name: (Number(searchQuery))}
             ]
         })
     
          res.json({
-            data:getreceipt,
-            message:"receipt loaded",
+            data:getdocument,
+            message:"document loaded",
             varient:"success"
          })
     
