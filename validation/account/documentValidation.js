7c3a8a5d69f9ae746f9b71b20241140e04e5cf5b
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-20 11:22:10 +0530

    documentNumber run

--- validation/account/documentValidation.js
+++ validation/account/documentValidation.js
@@ -3,7 +3,7 @@ const validateOnCreate = async(req,res,next) => {
     if(!req.body?.documentNumber){
 
        return res.status(400).json({
-            message: "",
+            message: "documentNumber",
             varient: "error"
         })
     }
