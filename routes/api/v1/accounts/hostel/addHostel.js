2111d0aaad8febc3133132da82e7be129d830dba
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-22 21:59:30 +0530

    get hostel run

--- routes/api/v1/accounts/hostel/addHostel.js
+++ routes/api/v1/accounts/hostel/addHostel.js
@@ -1,51 +1,125 @@
 const express = require("express");
-const Hostel = require("../../../../../Models/Accounts/Hostel");
+const Document = require("../../../../../Models/Accounts/Document");
 const router = express.Router();
 
-const {validateOnCreate, validateOnUpdate} = require("../../../../../validation/account/hostelValidation")
+const {validateOnCreate, validateOnUpdate} = require("../../../../../validation/account/documentValidation")
 
 //CRUD = crete Read Update Delete
 
 //@type POST
-//@routes/api/v1/accounts/hostel/addhostel
-//@des crete New hostel
+//@routes/api/v1/accounts/document/addDocument
+//@des crete New document
 //@access public
 
 router.post("/",validateOnCreate, async(req,res) =>{
 
+    try{
+        const documentObj = await getDocumentObj(req,"create")
 
-})
+     await new Document(documentObj)
+      .save();
+      
+      res.statusCode(201).json({
+        message: "Document Added",
+        varient : "success"
+      })
+
+    } catch(error){
+        console.log(error)
+        res.statusCode(500).json({
+        message: "Internal server error",
+        varient : "error"
+      })
+    }
+    
+
+}
+
+)
 //@type POST
-//@routes/api/v1/accounts/hostel/addhostel/id:
-//@des crete Update hostel
+//@routes/api/v1/accounts/document/addDocument/id:
+//@des crete Update document
 //@access public
 router.post("/:id",async (req,res) => {
+    
+    try{
+        const documentObj = await getDocumentObj(req,"update")
+
+        const document = await Document.findOneAndUpdate(
+            {id:req.params.id},
+            {$set: documentObj},
+            {new:true}
+        )
+        if(!document){
+            res.statusCode(500).json({
+                message: "document not found",
+                varient : "error"
+              })
+        }
+        res.statusCode(500).json({
+            message: "Document Updated Successfully",
+            varient : "error"
+          })
+     
+
+    } catch(error){
+        console.log(error)
+        res.statusCode(500).json({
+        message: "Internal server error",
+        varient : "error"
+      })
+    }
+    
 })
 
 //@type Delete
-//@route / api/v1/accounts/hostel/addhostel/deleteOne/id:
-//@des Delete hostel
+//@route / api/v1/accounts/document/adddocument/deleteOne/id:
+//@des Delete document
 //@access public
 
 router.delete("/deleteOne/:id",async(req,res) => {
-    })
 
-    
-async function gethostelObj(req,type){
+    try{
+        const document = await Document.findIdAndRemove(req.params.id);
+        if(!document){
+            res.statusCode(500).json({
+                message: "document not found",
+                varient : "error"
+              })
+        }
+        res.statusCode(500).json({
+            message: "Document Deleted Successfully",
+            varient : "error"
+          })
+     
 
-    let newHostel = {}
-    if(req.body.amount) {
-        newHostel.amount = req.body.amount
     }
-    if(req.body.note) {
-        newHostel.note = req.body.note
+    catch(error){
+        console.log(error)
+        res.statusCode(500).json({
+            message: "Internal Server error",
+            varient : "error"
+          })
+     
+
+    }
+    })
+    
+    
+async function getDocumentObj(req,type){
+
+    let newDocument = {}
+    if(req.body.number) {
+         document.number = req.body.number
+    }
+    if(req.body.name) {
+         document.name = req.body.name
     }
     if(req.body.date) {
-        newHostel.date = req.body.date
+         document.date = req.body.date
     }
-   
 
-    return newHostel
+    return newDocument
 }
     
 module.exports = router;
