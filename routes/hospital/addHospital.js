9083371c1f7f8a95ea5708c7f35a030d80e0c5a3
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-25 21:28:07 +0530

    addd run

--- routes/hospital/addHospital.js
+++ routes/hospital/addHospital.js
@@ -1,60 +1,125 @@
 const express = require("express");
-const Hospital = require("../../../../../Models/Accounts/Hospital");
+const Document = require("../../../../../Models/Accounts/Document");
 const router = express.Router();
 
-const {validateOnCreate, validateOnUpdate} = require("../../../../../validation/account/hospitalValidation")
+const {validateOnCreate, validateOnUpdate} = require("../../../../../validation/account/documentValidation")
 
 //CRUD = crete Read Update Delete
 
 //@type POST
-//@routes/api/v1/accounts/hospital/addhospital
-//@des crete New hospital
+//@routes/api/v1/accounts/document/addDocument
+//@des crete New document
 //@access public
 
 router.post("/",validateOnCreate, async(req,res) =>{
 
+    try{
+        const documentObj = await getDocumentObj(req,"create")
 
-})
+     await new Document(documentObj)
+      .save();
+      
+      res.statusCode(201).json({
+        message: "Document Added",
+        varient : "success"
+      })
+
+    } catch(error){
+        console.log(error)
+        res.statusCode(500).json({
+        message: "Internal server error",
+        varient : "error"
+      })
+    }
+    
+
+}
+
+)
 //@type POST
-//@routes/api/v1/accounts/hospital/addhospital/id:
-//@des crete Update hospital
+//@routes/api/v1/accounts/document/addDocument/id:
+//@des crete Update document
 //@access public
 router.post("/:id",async (req,res) => {
+    
+    try{
+        const documentObj = await getDocumentObj(req,"update")
+
+        const document = await Document.findOneAndUpdate(
+            {id:req.params.id},
+            {$set: documentObj},
+            {new:true}
+        )
+        if(!document){
+            res.statusCode(500).json({
+                message: "document not found",
+                varient : "error"
+              })
+        }
+        res.statusCode(500).json({
+            message: "Document Updated Successfully",
+            varient : "error"
+          })
+     
+
+    } catch(error){
+        console.log(error)
+        res.statusCode(500).json({
+        message: "Internal server error",
+        varient : "error"
+      })
+    }
+    
 })
 
 //@type Delete
-//@route / api/v1/accounts/hospital/addhospital/deleteOne/id:
-//@des Delete hospital
+//@route / api/v1/accounts/document/adddocument/deleteOne/id:
+//@des Delete document
 //@access public
 
 router.delete("/deleteOne/:id",async(req,res) => {
+
+    try{
+        const document = await Document.findIdAndRemove(req.params.id);
+        if(!document){
+            res.statusCode(500).json({
+                message: "document not found",
+                varient : "error"
+              })
+        }
+        res.statusCode(500).json({
+            message: "Document Deleted Successfully",
+            varient : "error"
+          })
+     
+
+    }
+    catch(error){
+        console.log(error)
+        res.statusCode(500).json({
+            message: "Internal Server error",
+            varient : "error"
+          })
+     
+
+    }
     })
-async function gethospitalObj(req,type){
+    
+    
+async function getDocumentObj(req,type){
 
-    let newHospital = {}
-    if(req.body.hospitalName) {
-        newHospital.hospitalName = req.body.hospitalName
+    let newDocument = {}
+    if(req.body.number) {
+         document.number = req.body.number
     }
-    if(req.body.patientName) {
-        newHospital.patientName = req.body.patientName
+    if(req.body.name) {
+         document.name = req.body.name
     }
-    if(req.body.patientAddress) {
-        newHospital.patientAddress = req.body.patientAddress
-    }
-    if(req.body.bloodHeart) {
-        newHospital.bloodHeart = req.body.bloodHeart
-    }
-    if(req.body.healthCheckUp) {
-        newHospital. healthCheckUp = req.body. healthCheckUp
-    }
-    if(req.body.hospitalFee) {
-        newHospital.hospitalFee = req.body.hospitalFee
-    }
-    if(req.body.comingDate) {
-        newHospital.comingDate = req.body.comingDate
+    if(req.body.date) {
+         document.date = req.body.date
     }
 
-    return newHospital
+    return newDocument
 }
     
 module.exports = router;
