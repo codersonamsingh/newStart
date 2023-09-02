6a2d3cf76352ae8581188d9518eb7ac1d4fac155
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-02 20:15:45 +0530

    getOffice run

---
+++ routes/api/v1/accounts/office.js/getOffice.js
@@ -0,0 +1,97 @@
+//Read = 1. Get ALL office , 2. get one office by Id, 3. Search Office with note or amount
+
+const express = require("express");
+const Office = require("../../../../../Models/Accounts/Office");
+const router = express.Router();
+
+//@type Get
+//@route /api/v1/accounts/office/getOffice/getAll
+//@des get all  office
+//@access public
+router.get("/getAll", async(req,res) => {
+
+try{ 
+    
+    const getOffice = await Office.find()
+
+
+     res.json({
+        data:getOffice,
+        message:"office loaded",
+        varient:"success"
+     })
+
+}catch(error){
+    console.log(error)
+
+    res.status(500).json({
+        message: "Internal Server error",
+        varient: "error"
+    })
+}
+})
+
+//@type Get
+//@route /api/v1/account/office/getOffice/getOne/:id
+//@des get one office
+//@access public
+router.get("/getOne/:id", async(req,res) => {
+
+    try{ 
+        
+        const getOffice = await Office.findById(req.params.id)
+    
+    
+         res.json({
+            data:getOffice,
+            message:"office loaded",
+            varient:"success"
+         })
+    
+    }catch(error){
+        console.log(error)
+    
+        res.status(500).json({
+            message: "Internal Server error",
+            varient: "error"
+        })
+    }
+    })
+
+//@type Get search
+//@route /api/v1/account/office/getOffice/search/:seachQuery
+//@des to search office
+//@access public   
+router.get("/search/:searchQuery", async(req,res) => {
+
+    try{ 
+        
+        const searchQuery = req.params.searchQuery
+
+        const getOffice = await Office.find({
+            $or:[
+                
+                {fatherName: new RegExp(searchQuery, "i")},
+                
+                {studentName: new RegExp(searchQuery, "i")},
+                {number: new RegExp(searchQuery, "i")},
+                
+            ]
+        })
+    
+         res.json({
+            data:getoffice,
+            message:"office loaded",
+            varient:"success"
+         })
+    
+    }catch(error){
+        console.log(error)
+    
+        res.status(500).json({
+            message: "Internal Server error",
+            varient: "error"
+        })
+    }
+    })
+module.exports = router 
