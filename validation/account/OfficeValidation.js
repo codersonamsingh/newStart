0ffedcd8ac48251f50e26255cdc1403c40be98f3
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-03 07:57:52 +0530

    validation run

--- validation/account/OfficeValidation.js
+++ validation/account/OfficeValidation.js
@@ -9,10 +9,8 @@ const validateOnCreate = async(req,res,next) => {
     }
     next();
 }
-
 const validateOnUpdate = async(req,res,next) => {
 
     next();
 }
-
 module.exports = {validateOnCreate, validateOnUpdate}
