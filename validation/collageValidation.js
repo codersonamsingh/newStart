879581673a9fce9704935f4043d78a0f6959b67a
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-07 17:07:11 +0530

    collag run

--- validation/collageValidation.js
+++ validation/collageValidation.js
@@ -1,6 +1,6 @@
 const validateOnCreate = async(req,res,next) => {
 
-    if(!req.body?.receiptNumber){
+    if(!req.body?.collageName){
 
        return res.status(400).json({
             message: "receiptNumber Required",
