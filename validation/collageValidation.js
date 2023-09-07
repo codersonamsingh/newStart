f11125b59811795f112c110f521d2dd614214277
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-07 17:25:21 +0530

    validation

--- validation/collageValidation.js
+++ validation/collageValidation.js
@@ -3,7 +3,7 @@ const validateOnCreate = async(req,res,next) => {
     if(!req.body?.collageName){
 
        return res.status(400).json({
-            message: " Required",
+            message: "collageName Required",
             varient: "error"
         })
     }
