28c917641ed0498f5842474df4580ebfae89e19b
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-15 22:31:55 +0530

    shop run

--- routes/api/v1/shopkeeper/addShopkeeper.js
+++ routes/api/v1/shopkeeper/addShopkeeper.js
@@ -120,7 +120,9 @@ async function getShopkeeperObj(req,type){
      if(req.body.gstNo) {
         newShopkeeper.gstNo = req.body.gstNo
     }
-    
+    if(req.body.gstNo) {
+        newShopkeeper.gstNo = req.body.gstNo
+    }
      if(req.body.phoneNumber) {
         newShopkeeper.phoneNumber = req.body. phoneNumber
     }
