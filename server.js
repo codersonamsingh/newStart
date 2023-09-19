24eb0d17fc38683f12599308e29feca9c0240c2b
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-19 18:02:00 +0530

    sravv run

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use("/api/v1/accounts/office/addOffice",addOffice)
 app.use("/api/v1/accounts/office/getOffice",getOffice)
 app.use("/api/v1/accounts/collage/addCollage",addCollage)
 app.use("/api/v1/accounts/collage/getCollage",getCollage)
-app.use("/api/api/v1/accounts/School/addSchool",addSchool)
+app.use("/api/api/v1/accounts/school/addSchool",addSchool)
 app.use("/api/api/v1/accounts/school/getSchool",getSchool)
 app.use("/api/api/v1/accounts/School/getSchool",getSchool)
 
