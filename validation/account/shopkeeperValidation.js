89d212b6107ee55fc2cf1eb931bb257e77ff469a
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-20 16:31:02 +0530

    nam  run

--- validation/account/shopkeeperValidation.js
+++ validation/account/shopkeeperValidation.js
@@ -1,6 +1,6 @@
 const validateOnCreate = async(req,res,next) => {
 
-    if(!req.body?.schoolName){
+    if(!req.body?.shopkeeperName){
 
        return res.status(400).json({
             message: "schoolName Required",
