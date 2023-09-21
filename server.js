3235b5340c6f04b768546dd2af7cc69cc72e21e2
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-21 17:04:06 +0530

    shop run

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use("/api/v1/accounts/collage/getCollage",getCollage)
 app.use("/api/api/v1/accounts/school/addSchool",addSchool)
 app.use("/api/api/v1/accounts/school/getSchool",getSchool)
 app.use("/api/api/v1/accounts/shopkeeper/addShopkeeper",addShopkeeper)
-app.use("/api/api/v1/accounts/Shopkeeper/getShopkeeper",getShopkeeper)
+app.use("/api/api/v1/accounts/shopkeeper/getShopkeeper",getShopkeeper)
 
 
 //data base connection
