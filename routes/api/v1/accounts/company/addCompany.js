b5fa7937201304e9ad3be2eb758c230ed53f3683
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-23 20:52:15 +0530

    regulator run

--- routes/api/v1/accounts/company/addCompany.js
+++ routes/api/v1/accounts/company/addCompany.js
@@ -114,8 +114,8 @@ async function getCompanyObj(req,type){
     if(req.body.incomeTax) {
         newCompany.incomeTax = req.body.incomeTax
     }
-     if(req.body.itemRate) {
-        newCompany.itemRate = req.body.itemRate
+     if(req.body. regulator) {
+        newCompany. regulator = req.body. regulator
     }
      if(req.body.gstNo) {
         newCompany.gstNo = req.body.gstNo
