13e65c183a37b67eaf828161a7ae03dc313b5669
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-24 12:11:09 +0530

    companyValidation run

---
+++ validation/account/companyValidation.js
@@ -0,0 +1,16 @@
+const validateOnCreate = async(req,res,next) => {
+
+    if(!req.body?.shopkeeperName){
+
+       return res.status(400).json({
+            message: "shopkeeperName Required",
+            varient: "error"
+        })
+    }
+    next();
+}
+const validateOnUpdate = async(req,res,next) => {
+
+    next();
+}
+module.exports = {validateOnCreate, validateOnUpdate}
